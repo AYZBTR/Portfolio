dbe65e791f75b6015a14ac9ee16a6578de163b40
AYZBTR <bhattaraiaayush.78@gmail.com>
2025-11-21 13:15:24 +0200

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Dashboard from "./pages/Admin/Dashboard";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/admin" element={<Dashboard />} />
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
+  );
+}
